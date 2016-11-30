9b0b46749b884e873314b10950758b063368b9ce
Paul Hudson <paul.hudson@gmail.com>
2016-11-30 22:23:13 +0000

    Changed @available to #available to avoid errors for pre-10.11 targets.

--- Sources/SwiftSlug.swift
+++ Sources/SwiftSlug.swift
@@ -1,39 +1,49 @@
 import Foundation
 
 enum SlugConversionError: Error {
-	case failedToConvert
+    case failedToConvert
 }
 
 extension String {
-	private static let slugSafeCharacters = CharacterSet(charactersIn: "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-")
+    private static let slugSafeCharacters = CharacterSet(charactersIn: "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-")
 
-	@available(OSX 10.11, *)
-	public func convertedToSlug() throws -> String {
-		var result: String? = nil
+    private func convertedToSlugBackCompat() -> String? {
+        // On Linux StringTransform doesn't exist and CFStringTransform causes all sorts
+        // of problems because of bridging issues using CFMutableString – d'oh.
+        // So we're going to do the only thing possible: dump to ASCII and hope for the best
+        if let data = self.data(using: .ascii, allowLossyConversion: true) {
+            if let str = String(data: data, encoding: .ascii) {
+                let urlComponents = str.lowercased().components(separatedBy: String.slugSafeCharacters.inverted)
+                return urlComponents.filter { $0 != "" }.joined(separator: "-")
+            }
+        }
 
-		#if os(Linux)
-			// On Linux StringTransform doesn't exist and CFStringTransform causes all sorts
-			// of problems because of bridging issues using CFMutableString – d'oh.
-			// So we're going to do the only thing possible: dump to ASCII and hope for the best
-			if let data = self.data(using: .ascii, allowLossyConversion: true) {
-				if let str = String(data: data, encoding: .ascii) {
-					let urlComponents = str.lowercased().components(separatedBy: String.slugSafeCharacters.inverted)
-					result = urlComponents.filter { $0 != "" }.joined(separator: "-")
-				}
-			}
-		#else
-			if let latin = self.applyingTransform(StringTransform("Any-Latin; Latin-ASCII; Lower;"), reverse: false) {
-				let urlComponents = latin.components(separatedBy: String.slugSafeCharacters.inverted)
-				result = urlComponents.filter { $0 != "" }.joined(separator: "-")
-			}
-		#endif
+        // still here? Something went disastrously wrong!
+        return nil
+    }
 
-		if let result = result {
-			if result.characters.count > 0 {
-				return result
-			}
-		}
+    public func convertedToSlug() throws -> String {
+        var result: String? = nil
 
-		throw SlugConversionError.failedToConvert
-	}
+        #if os(Linux)
+            result = convertedToSlugBackCompat()
+        #else
+            if #available(OSX 10.11, *) {
+                if let latin = self.applyingTransform(StringTransform("Any-Latin; Latin-ASCII; Lower;"), reverse: false) {
+                    let urlComponents = latin.components(separatedBy: String.slugSafeCharacters.inverted)
+                    result = urlComponents.filter { $0 != "" }.joined(separator: "-")
+                }
+            } else {
+                result = convertedToSlugBackCompat()
+            }
+        #endif
+
+        if let result = result {
+            if result.characters.count > 0 {
+                return result
+            }
+        }
+
+        throw SlugConversionError.failedToConvert
+    }
 }
